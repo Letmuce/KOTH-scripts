22b592cafb8b97c40657e401574fd78c113817f2
Alex Arbutiski <93349456+Letmuce@users.noreply.github.com>
2022-04-14 17:35:49 -0400

    Create Apache_port_changer.py

---
+++ Apache_port_changer.py
@@ -0,0 +1,26 @@
+"""
+This script changes the port Apache runs on every few seconds
+"""
+
+import random
+import time
+import os
+
+while True:
+  
+  with open('/etc/apache2/ports.conf') as file:
+    data = file.readlines()
+  
+  # replace port
+  data[4] = 'Listen {}'.format(str(random.randrange(5000,65000,1)))
+  
+  # write to file
+  with open('/etc/apache2/ports.conf', 'w') as file:
+    file.writelines(data)
+  
+  
+  # restart Apache
+  os.system('/etc/init.d/apache2 restart')
+
+  # delay (keep it above 3 so Apache has time to reset)
+  time.sleep(4)
