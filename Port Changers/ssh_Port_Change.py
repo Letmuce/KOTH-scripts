0f7eb3f02d631f25058592c9cfd7a9f4c8d6929d
Alex Arbutiski <93349456+Letmuce@users.noreply.github.com>
2022-04-27 13:51:57 -0400

    Update ssh_Port_Change.py

--- Port Changers/ssh_Port_Change.py
+++ Port Changers/ssh_Port_Change.py
@@ -18,7 +18,7 @@ while True:
   for num, line in enumerate(data, 1):
     if text in line:
       data[num - 1] = 'Port {}\n'.format(str(random.randrange(5000,65000,1)))
-  data.close()
+    file.close()
   
   # write to file
   with open('/etc/ssh/sshd_config', 'w') as file:
